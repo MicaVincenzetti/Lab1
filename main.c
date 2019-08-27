c2f0e0fccf81e43ddc1dd859371550b2ef340350
MicaVincenzetti <52495057+MicaVincenzetti@users.noreply.github.com>
2019-08-27 12:25:24 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+int main()
+{
+    char opcion;
+    do
+    {
+        printf("1. ALTA Cliente\n");
+        printf("2. BAJA Cliente\n");
+        printf("3. MODIFICAR Cliente\n");
+        printf("4. MOSTRAR Clientes\n");
+        printf("5. SALIR\n");
+        printf("Elija una opcion: ");
+        fflush(stdin);
+        scanf("%c", &opcion);
+        switch(opcion)
+        {
+            case '1':
+                printf("Estoy dando de alta!!!\n");
+                break;
+            case '2':
+                printf("Estoy dando de baja!!!\n");
+                break;
+            case '3':
+                printf("Estoy modificando!!!\n");
+                break;
+            case '4':
+                printf("Estoy mostrando!!!\n");
+                break;
+            case '5':
+                printf("Estoy saliendo!!!\n");
+            default:
+                printf("Opcion incorrecta!!!\n");
+
+        }
+    system("pause");
+    system("cls");
+    }
+    while(opcion!='5');
+
+    return 0;
+}
+*/ //Ejemplo de menu//
+
+
+
+int sumaNumeros (int , int );
+int main()
+{
+
+
+
+
+    {
+
+    int num1;
+    int num2;
+    int suma;
+    int total;
+
+    printf("ingrese numero a sumar: ");
+    scanf("%d",& num1);
+
+    printf("ingrese numero a sumar: ");
+    scanf("%d",&num2);
+
+    total =suma(num1,num2);
+
+    printf("La suma es %d",total);
+
+
+    }
+
+
+    int total (int num1, int num2);
+
+        {
+            int suma;
+            suma = num1 + num2;
+            return suma;
+        }
+}
